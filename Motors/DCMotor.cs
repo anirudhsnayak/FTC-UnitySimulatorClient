2189a230e03280cc68f6a36948934c8a2274b03b
AnonThief <anirudhsnayak@gmail.com>
2021-01-31 13:49:47 -0800

    Add files via upload

---
+++ Motors/DCMotor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DCMotor : MonoBehaviour
+{
+    const int TICKS_PER_ROT = 1120;
+    const int REFRESH_RATE = 25;
+
+    public new HingeJoint hingeJoint;
+    public long ticks;
+    public string Tag;
+
+    float prevAngle;
+
+    void Start()
+    {
+        hingeJoint.useMotor = true;
+        CommandHandler.RegisterJoint(Tag, hingeJoint);
+        StartCoroutine(UpdateSensor()); //Encoder
+    }
+    IEnumerator UpdateSensor()
+    {
+        while (true)
+        {
+            ticks += ticksSinceLastMeasurement();
+            if (SimulatorHandler.sc != null)
+            {
+                SimulatorHandler.sc.SendMessage("S;" + ((int)SensorCommand.EncoderSensor) + ";" + Tag + ";" + ticks + ";");
+            }
+            yield return new WaitForSeconds(1 / REFRESH_RATE);
+        }
+    }
+    int ticksSinceLastMeasurement() //reallly rough, need to get more accurate results
+    {
+        float dt = 1 / (float)REFRESH_RATE;
+        int scaleTicks = (int)(hingeJoint.velocity * dt) * (TICKS_PER_ROT / 360);
+        return scaleTicks;
+    }
+}
---
+++ Sensors/IMUSensor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMUSensor : MonoBehaviour
+{
+    public string Tag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UpdateSensor());
+    }
+    IEnumerator UpdateSensor()
+    {
+        while (true)
+        {
+            if (SimulatorHandler.sc != null)
+            {
+                SimulatorHandler.sc.SendMessage("S;" + ((int)SensorCommand.IMUSensor) + ";" + Tag + ";" + formatArgument(transform.position) + formatArgument(transform.rotation.eulerAngles));
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    string formatArgument(Vector3 vector3)
+    {
+        return vector3.x.ToString() + ";" + vector3.y.ToString() + ";" + vector3.z.ToString() + ";";
+    }
+}
